a8fc1435eeb749e10c6a17274c08da0935bb5050
Natalia Nascimento <natalia.nascc@gmail.com>
2021-09-13 17:48:29 -0300

    Alterações

--- bonificacoes.php
+++ bonificacoes.php
@@ -2,14 +2,20 @@
 
 require_once 'autoload.php';
 
-use Alura\Banco\Modelo\{Funcionario, CPF};
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Funcionario\{Diretor, Gerente, Desenvolvedor};
 use Alura\Banco\Service\ControladorDeBonificacoes;
 
-$umFuncionario = new Funcionario('Vinicius Dias', new CPF('123.456.789-10'), 'Desenvolvedor', 1000);
-$umaFuncionaria = new Funcionario('Patricia Dias', new CPF('987.654.321-10'), 'Gerente', 3000);
+$umFuncionario = new Desenvolvedor('Vinicius Dias', new CPF('123.456.789-10'), 1000);
+$umaFuncionaria = new Gerente('Patricia Dias', new CPF('987.654.321-10'), 3000);
+$umDiretor = new Diretor('Ana Dias', new CPF('123.654.789-10'), 5000);
+
+$umFuncionario->sobeDeNivel();
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
+$controlador->adicionaBonificacaoDe($umDiretor);
+
 
 echo $controlador->recuperaTotal();
